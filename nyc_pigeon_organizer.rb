1fd8ee0fbffa7f58540acd071e5a3537546262a5
Muhammad Hassan <mhassan.work99@gmail.com>
2020-05-22 02:54:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
     value.each do |inner_key, names|
       names.each do |name|
         if !final_array[name]
-          final_array[name]
+          final_array[name] = {}
         
       end
     end  
